90840b7753b0489e46ec929ea4aae371171b6142
MarioR4 <a01197707@itesm.mx>
2022-10-05 08:30:45 -0500

    calculadora

--- mySecondApp/mySecondApp/ViewController.swift
+++ mySecondApp/mySecondApp/ViewController.swift
@@ -11,6 +11,74 @@ class ViewController: UIViewController {
 
     @IBOutlet var usuarioTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var numeroSuperior: UITextField!
+    @IBOutlet var numeroInferior: UITextField!
+    
+    @IBAction func Suma(_ sender: UIButton) {
+        let num1 = Double(numeroSuperior.text ?? "0")
+        let num2 = Double(numeroInferior.text ?? "0")
+        var mensaje = "poner ambas"
+        
+        // Control de Variables
+        if (num1 != nil && num2 != nil) {
+            let resultado = (num1!) + (num2!)
+            mensaje = String(resultado)
+        }
+        
+        let alert = UIAlertController(title: "Alerta!!", message: mensaje, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Cerrar", style: UIAlertAction.Style.cancel))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func Resta(_ sender: UIButton) {
+        let num1 = Double(numeroSuperior.text ?? "0")
+        let num2 = Double(numeroInferior.text ?? "0")
+        var mensaje = "poner ambas"
+        
+        // Control de Variables
+        if (num1 != nil && num2 != nil) {
+            let resultado = (num1!) - (num2!)
+            mensaje = String(resultado)
+        }
+        
+        let alert = UIAlertController(title: "Alerta!!", message: mensaje, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Cerrar", style: UIAlertAction.Style.cancel))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func Multiplicacion(_ sender: UIButton) {
+        let num1 = Double(numeroSuperior.text ?? "0")
+        let num2 = Double(numeroInferior.text ?? "0")
+        var mensaje = "poner ambas"
+        
+        // Control de Variables
+        if (num1 != nil && num2 != nil) {
+            let resultado = (num1!) * (num2!)
+            mensaje = String(resultado)
+        }
+        
+        let alert = UIAlertController(title: "Alerta!!", message: mensaje, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Cerrar", style: UIAlertAction.Style.cancel))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func division(_ sender: UIButton) {
+        let dividendo = Double(numeroSuperior.text ?? "0")
+        let divisor = Double(numeroInferior.text ?? "0")
+        var mensaje = "poner ambas"
+        
+        // Control de Variables
+        //if (dividendo != nil && divisor != nil) {
+          //  let resultado = (dividendo!) / (divisor!)
+            //mensaje = String(resultado)
+        //}
+        // Control de excepciones
+        
+        let alert = UIAlertController(title: "Alerta!!", message: mensaje, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Cerrar", style: UIAlertAction.Style.cancel))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func botonUnoClick(_ sender: UIButton) {
         let alert = UIAlertController(title: "Alerta!!", message: usuarioTextField.text, preferredStyle: UIAlertController.Style.alert)
         
