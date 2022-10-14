ddb44322f8cd8e8e8f32f370426390f5c580f131
MarioR4 <a01197707@itesm.mx>
2022-10-14 16:13:35 -0500

    Calculadora lista para entregar

---
+++ Calculadora/Calculadora/ImageViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ImageViewController.swift
+//  Calculadora
+//
+//  Created by MARIO ISAI ROBLES LOZANO on 14/10/22.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func backButton(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
